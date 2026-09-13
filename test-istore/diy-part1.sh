ede89985c487c789dc43ba588f393e564130fbdb
googlewmb <58500194+googlewmb@users.noreply.github.com>
2026-09-13 21:57:04 +0800

    Update diy-part1.sh

--- test-istore/diy-part1.sh
+++ test-istore/diy-part1.sh
@@ -5,8 +5,8 @@
 #
 
 # 设置 conntrack 最大连接数为 655550
-sed -i '/^[[:space:]]*net\.netfilter\.nf_conntrack_max[[:space:]]*=/d' package/base-files/files/etc/sysctl.conf
-echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sysctl.conf
+#sed -i '/^[[:space:]]*net\.netfilter\.nf_conntrack_max[[:space:]]*=/d' package/base-files/files/etc/sysctl.conf
+#echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sysctl.conf
 
 # 添加软件源
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
@@ -21,99 +21,99 @@ echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sys
 
 #rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-passwall2,luci-app-openclash,luci-app-homeproxy}
 #rm -rf ./feeds/luci/applications/{luci-app-lucky,luci-app-timecontrol,luci-app-mosdns}
-rm -rf ./feeds/luci/applications/{luci-app-nikki,luci-app-momo,luci-app-daed}
+#rm -rf ./feeds/luci/applications/{luci-app-nikki,luci-app-momo,luci-app-daed}
 
 # Golang
-rm -rf feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
 
-git clone --depth 1 -b 1.26 \
-https://github.com/kenzok8/golang \
-feeds/packages/lang/golang
+#git clone --depth 1 -b 1.26 \
+#https://github.com/kenzok8/golang \
+#feeds/packages/lang/golang
 
 # PassWall 依赖
 #rm -rf package/passwall-packages
 
-git clone --depth 1 \
-https://github.com/Openwrt-Passwall/openwrt-passwall-packages \
-package/passwall-packages
+#git clone --depth 1 \
+#https://github.com/Openwrt-Passwall/openwrt-passwall-packages \
+#package/passwall-packages
 
 # 第三方软件源
-mkdir -p package/small
-cd package/small
+#mkdir -p package/small
+#cd package/small
 
 # PassWall
-git clone -b main --depth 1 \
-https://github.com/Openwrt-Passwall/openwrt-passwall.git
+#git clone -b main --depth 1 \
+#https://github.com/Openwrt-Passwall/openwrt-passwall.git
 
 # PassWall2
-git clone -b main --depth 1 \
-https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+#git clone -b main --depth 1 \
+#https://github.com/Openwrt-Passwall/openwrt-passwall2.git
 
 # SmartDNS LuCI
-#git clone -b master --depth 1 \
-https://github.com/pymumu/luci-app-smartdns.git
+##git clone -b master --depth 1 \
+#https://github.com/pymumu/luci-app-smartdns.git
 
 # MosDNS
-git clone -b v5 --depth 1 \
-https://github.com/sbwml/luci-app-mosdns.git
+#git clone -b v5 --depth 1 \
+#https://github.com/sbwml/luci-app-mosdns.git
 
 # OpenClash
-git clone -b master --depth 1 \
-https://github.com/vernesong/OpenClash.git
+#git clone -b master --depth 1 \
+#https://github.com/vernesong/OpenClash.git
 
 # HomeProxy
-git clone -b master --depth 1 \
-https://github.com/immortalwrt/homeproxy.git
+#git clone -b master --depth 1 \
+#https://github.com/immortalwrt/homeproxy.git
 
 # Lucky
-git clone -b main --depth 1 \
-https://github.com/gdy666/luci-app-lucky.git
+#git clone -b main --depth 1 \
+#https://github.com/gdy666/luci-app-lucky.git
 
 # TimeControl
-git clone -b main --depth 1 \
-https://github.com/sirpdboy/luci-app-timecontrol.git
+#git clone -b main --depth 1 \
+#https://github.com/sirpdboy/luci-app-timecontrol.git
 
 # NetSpeedTest
-git clone -b master --depth 1 \
-https://github.com/sirpdboy/luci-app-netspeedtest.git
+#git clone -b master --depth 1 \
+#https://github.com/sirpdboy/luci-app-netspeedtest.git
 
 # Nikki
-git clone -b main --depth 1 \
-https://github.com/nikkinikki-org/OpenWrt-nikki.git
+#git clone -b main --depth 1 \
+#https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 # Momo
-git clone -b main --depth 1 \
-https://github.com/nikkinikki-org/OpenWrt-momo.git
+#git clone -b main --depth 1 \
+#https://github.com/nikkinikki-org/OpenWrt-momo.git
 
 # Daed
-git clone -b master --depth 1 \
-https://github.com/QiuSimons/luci-app-daed.git
+#git clone -b master --depth 1 \
+#https://github.com/QiuSimons/luci-app-daed.git
 
 # Aurora Theme
-git clone -b master --depth 1 \
-https://github.com/eamonxg/luci-theme-aurora.git
+#git clone -b master --depth 1 \
+#https://github.com/eamonxg/luci-theme-aurora.git
 
 # VIKINGYFY Packages
-git clone -b main --depth 1 \
-https://github.com/VIKINGYFY/packages.git
+#git clone -b main --depth 1 \
+#https://github.com/VIKINGYFY/packages.git
 
 # AdGuardHome
-# git clone -b 2024.09.05 --depth 1 \
-# https://github.com/XiaoBinin/luci-app-adguardhome.git
+## git clone -b 2024.09.05 --depth 1 \
+## https://github.com/XiaoBinin/luci-app-adguardhome.git
 
 # SSRP
-# git clone -b master --depth 1 \
-# https://github.com/fw876/helloworld.git
+## git clone -b master --depth 1 \
+## https://github.com/fw876/helloworld.git
 
 # Modem
-# git clone -b main --depth 1 \
-# https://github.com/FUjr/modem_feeds.git
+## git clone -b main --depth 1 \
+## https://github.com/FUjr/modem_feeds.git
 
-cd ../..
+#cd ../..
 
 # 更新 feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 
 # 自动添加 LuCI 中文语言包
 for pkg in $(grep '^CONFIG_PACKAGE_luci-app-.*=y' .config | sed 's/^CONFIG_PACKAGE_//;s/=y//'); do
@@ -130,4 +130,4 @@ for pkg in $(grep '^CONFIG_PACKAGE_luci-app-.*=y' .config | sed 's/^CONFIG_PACKA
 done
 
 # 修正配置
-make defconfig
+#make defconfig
